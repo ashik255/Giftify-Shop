6395120e13e16774685f887bf11eb5d6eba93ad3
ashik255 <ariful15-11556@diu.edu.bd>
2022-06-17 03:12:39 +0600

    fakedata

--- src/fakedata.js
+++ src/fakedata.js
@@ -1,151 +1,150 @@
 const shopdata = [
-    {
-        id:1,
-        name:'Gift Box (GB 809)',
-        price: '1,999.00৳' ,
-        img:"https://upohar.xyz/wp-content/uploads/2022/02/274359611_320092723484292_361948910754683011_n-600x600.jpg.webp",
-        categories:"gift"
-    },
-    {
-        id:2,
-        name:'Gift Box (GB 808)',
-        price:'3,499.00৳ ',
-        img:"https://upohar.xyz/wp-content/uploads/2022/02/274485403_320086966818201_8348248923367114499_n-600x600.jpg.webp",
-        categories:"gift"
-    },
-    {
-        id:3,
-        name:'Gift Box (GB-830)',
-        price:'2,699.00৳ ',
-        img:"https://upohar.xyz/wp-content/uploads/2022/06/286581255_135289659114294_5735359531278147069_n-600x600.jpg.webp",
-        categories:"gift"
-    },
-    {
-        id:4,
-        name:'Gift Box (GB-827)',
-        price:'1,750.00৳ ',
-        img:"https://upohar.xyz/wp-content/uploads/2022/06/285635811_135264772450116_1926340316190623982_n-600x600.jpg.webp",
-        categories:"gift"
-    },
-    {
-        id:5,
-        name:'Tiramisu Cake',
-        price:'1,350.00৳',
-        img:"https://upohar.xyz/wp-content/uploads/2022/06/tiramisu-600x600.jpg.webp",
-        categories:"food"
-    },
-    {
-        id:6,
-        name:'Mocha Cake',
-        price:'1,050.00৳',
-        img:"https://upohar.xyz/wp-content/uploads/2022/06/mocha-cake-600x600.jpg.webp",
-        categories:"food"
-    },
-    {
-        id:7,
-        name:'Nutella Oreo Cheesecake',
-        price:'1,450.00৳',
-        img:"https://upohar.xyz/wp-content/uploads/2022/06/nutella-oreo-cheese-cake-600x600.png.webp",
-        categories:"food"
-    },
-    {
-        id:8,
-        name:'Chocolate Cheese Cake',
-        price:'1,350.00৳',
-        img:"https://upohar.xyz/wp-content/uploads/2022/06/chocolate-cheese-cake-600x600.png.webp",
-        categories:"food"
-    },
-    {
-        id:9,
-        name:'Coin Plant(Pilea peperomioides)',
-        price:'334.00৳ ',
-        img:"https://upohar.xyz/wp-content/uploads/2022/03/InShot_20220222_200414191-600x600.jpg.webp",
-        categories:"plants"
-    },
-    {
-        id:10,
-        name:"Lemon ball Cactus",
-        price:"557.00৳",
-        img:"https://upohar.xyz/wp-content/uploads/2022/03/InShot_20220318_161954693-scaled-1-600x600.jpg.webp",
-        categories:"plants"
-    },
-    {
-        id:11,
-        name:"Huernia Macrocarpa",
-        price:"527.00৳",
-        img:"https://upohar.xyz/wp-content/uploads/2022/03/InShot_20220309_003614045-600x600.jpg.webp",
-        categories:"plans"
-    },
-    {
-        id:12,
-        name:"Big Snake Plant",
-        price:"1,338.00৳ ",
-        img:"https://upohar.xyz/wp-content/uploads/2022/03/InShot_20220308_225858663-600x600.jpg.webp",
-        categories:""
-    },
-    {
-        id:13,
-        name:"All Occasion Gift Box-2 (AOGB-2)",
-        price:"1,099.00৳ ",
-        img:"https://upohar.xyz/wp-content/uploads/2022/01/IMG_8778-scaled-600x600.jpg",
-        categories:"gift"
-    },
-    {
-        id:14,
-        name:"Wild Lemongrass - Scented Candle ",
-        price:" 699.00৳ ",
-        img:"https://upohar.xyz/wp-content/uploads/2021/10/IMG_9620-2-600x600.jpg.webp",
-        categories:"gift"
-    },
-    {
-        id:15,
-        name:"Midnight Jasmine - Scented Candle",
-        price:"699.00৳",
-        img:"https://upohar.xyz/wp-content/uploads/2021/10/IMG_9596-600x600.jpg.webp",
-        categories:"gift"
-    },
-    {
-        id:16,
-        name:"FRAGRANT ROSE, NECKLACE & RING GIFT BOX",
-        price:"749.00৳ ",
-        img:"https://upohar.xyz/wp-content/uploads/2021/09/2021-08-30-21-05-30-600x600.jpg.webp",
-        categories:"gift"
-    },
-    {
-        id:17,
-        name:"Wooden Gift Box with custom engraved",
-        price:"2,450.00৳ ",
-        img:"https://upohar.xyz/wp-content/uploads/2022/01/2-2-600x600.jpg.webp",
-        categories:"gift"
-    },
-    {
-        id:18,
-        name:"Rolex_Oyster Prepetual-Silver",
-        price:"1,800.00৳",
-        img:"https://upohar.xyz/wp-content/uploads/2021/10/Rolex-Oyster-prepetual_1800-600x600.jpg.webp",
-        categories:"watch"
-    },
-    {
-        id:19,
-        name:"Emporio_Armani-Silver",
-        price:"2,250.00৳",
-        img:"https://upohar.xyz/wp-content/uploads/2021/10/Emporio-armani_2250.3-600x600.jpg.webp",
-        categories:"watch"
-    },
-    {
-        id:20,
-        name:"Fossil-Brown-Black : Chronograph Watch",
-        price:"2,400.00৳",
-        img:"https://upohar.xyz/wp-content/uploads/2021/05/e2a3e254-89f0-4552-8503-ad2171731704-600x600.jpg.webp",
-        categories:"watch"
-    },
-    {
-        id:21,
-        name:"Flammen Electric LED 3D Fire Lamp",
-        price:"1,300.00৳",
-        img:"https://upohar.xyz/wp-content/uploads/2021/05/2021-05-29-18-04-43.jpg.webp",
-        categories:"gift"
-    },
-
     
+        {
+            "id":1,
+            "name":"Gift Box (GB 809)",
+            "price":"1,999.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/02/274359611_320092723484292_361948910754683011_n-600x600.jpg.webp",
+            "categories":"gift"
+        },
+        {
+            "id":2,
+            "name":"Gift Box (GB 808",
+            "price":"3,499.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/02/274485403_320086966818201_8348248923367114499_n-600x600.jpg.webp",
+            "categories":"gift"
+        },
+        {
+            "id":3,
+            "name":"Gift Box (GB-830)",
+            "price":"2,699.00৳ ",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/06/286581255_135289659114294_5735359531278147069_n-600x600.jpg.webp",
+            "categories":"gift"
+        },
+        {
+            "id":4,
+            "name":"Gift Box (GB-827)",
+            "price":"1,750.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/06/285635811_135264772450116_1926340316190623982_n-600x600.jpg.webp",
+            "categories":"gift"
+        },
+        {
+            "id":5,
+            "name":"Tiramisu Cake",
+            "price":"1,350.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/06/tiramisu-600x600.jpg.webp",
+            "categories":"food"
+        },
+        {
+            "id":6,
+            "name":"Mocha Cake",
+            "price":"1,050.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/06/mocha-cake-600x600.jpg.webp",
+            "categories":"food"
+        },
+        {
+            "id":7,
+            "name":"Nutella Oreo Cheesecake",
+            "price":"1,450.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/06/nutella-oreo-cheese-cake-600x600.png.webp",
+            "categories":"food"
+        },
+        {
+            "id":8,
+            "name":"Chocolate Cheese Cake",
+            "price":"1,350.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/06/chocolate-cheese-cake-600x600.png.webp",
+            "categories":"food"
+        },
+        {
+            "id":9,
+            "name":"Coin Plant(Pilea peperomioides)",
+            "price":"334.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/03/InShot_20220222_200414191-600x600.jpg.webp",
+            "categories":"plants"
+        },
+        {
+            "id":10,
+            "name":"Lemon ball Cactus",
+            "price":"557.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/03/InShot_20220318_161954693-scaled-1-600x600.jpg.webp",
+            "categories":"plants"
+        },
+        {
+            "id":11,
+            "name":"Huernia Macrocarpa",
+            "price":"527.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/03/InShot_20220309_003614045-600x600.jpg.webp",
+            "categories":"plants"
+        },
+        {
+            "id":12,
+            "name":"Big Snake Plant",
+            "price":"1,338.00৳ ",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/03/InShot_20220308_225858663-600x600.jpg.webp",
+            "categories":"plants"
+        },
+        {
+            "id":13,
+            "name":"All Occasion Gift Box-2 (AOGB-2)",
+            "price":"1,099.00৳ ",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/01/IMG_8778-scaled-600x600.jpg",
+            "categories":"gift"
+        },
+        {
+            "id":14,
+            "name":"Wild Lemongrass - Scented Candle ",
+            "price":" 699.00৳ ",
+            "img":"https://upohar.xyz/wp-content/uploads/2021/10/IMG_9620-2-600x600.jpg.webp",
+            "categories":"gift"
+        },
+        {
+            "id":15,
+            "name":"Midnight Jasmine - Scented Candle",
+            "price":"699.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2021/10/IMG_9596-600x600.jpg.webp",
+            "categories":"gift"
+        },
+        {
+            "id":16,
+            "name":"FRAGRANT ROSE, NECKLACE & RING GIFT BOX",
+            "price":"749.00৳ ",
+            "img":"https://upohar.xyz/wp-content/uploads/2021/09/2021-08-30-21-05-30-600x600.jpg.webp",
+            "categories":"gift"
+        },
+        {
+            "id":17,
+            "name":"Wooden Gift Box with custom engraved",
+            "price":"2,450.00৳ ",
+            "img":"https://upohar.xyz/wp-content/uploads/2022/01/2-2-600x600.jpg.webp",
+            "categories":"gift"
+        },
+        {
+            "id":18,
+            "name":"Rolex_Oyster Prepetual-Silver",
+            "price":"1,800.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2021/10/Rolex-Oyster-prepetual_1800-600x600.jpg.webp",
+            "categories":"watch"
+        },
+        {
+            "id":19,
+            "name":"Emporio_Armani-Silver",
+            "price":"2,250.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2021/10/Emporio-armani_2250.3-600x600.jpg.webp",
+            "categories":"watch"
+        },
+        {
+            "id":20,
+            "name":"Fossil-Brown-Black : Chronograph Watch",
+            "price":"2,400.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2021/05/e2a3e254-89f0-4552-8503-ad2171731704-600x600.jpg.webp",
+            "categories":"watch"
+        },
+        {
+            "id":21,
+            "name":"Flammen Electric LED 3D Fire Lamp",
+            "price":"1,300.00৳",
+            "img":"https://upohar.xyz/wp-content/uploads/2021/05/2021-05-29-18-04-43.jpg.webp",
+            "categories":"gift"
+        }
 ]
